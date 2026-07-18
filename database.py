7ab83d1c8f6a7834e62f43a89fd22295978d33d7
Ben Mo Chen <benmochen@gmail.com>
2026-07-18 11:16:16 -0400

    Add get_db session dependency generator

--- database.py
+++ database.py
@@ -9,3 +9,9 @@ SessionLocal = sessionmaker(bind=engine)
 class Base(DeclarativeBase):
     pass
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
